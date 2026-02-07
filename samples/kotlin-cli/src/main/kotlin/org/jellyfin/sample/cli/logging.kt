b2cff359d94721815def38f71c40b20afb4a05fe
PeterCDMcLean <peter.cd.mclean@gmail.com>
2026-02-07 07:11:17 -0500

    Delete samples directory

--- samples/kotlin-cli/src/main/kotlin/org/jellyfin/sample/cli/logging.kt
+++
@@ -1,9 +0,0 @@
-package org.jellyfin.sample.cli
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import kotlin.properties.ReadOnlyProperty
-
-fun logger(): ReadOnlyProperty<Any?, Logger> = ReadOnlyProperty { thisRef, _ ->
-	LoggerFactory.getLogger(thisRef?.javaClass)
-}
